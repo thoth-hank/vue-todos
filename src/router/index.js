c84e2fb3f3a162616fea8d59adc69e7e7e2433ce
Hank <game8130@gmail.com>
2018-10-26 13:56:18 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue' // 導入Vue
 import Router from 'vue-router' // 導入vue-router 庫
-import layouts from '@/components/layouts' // 導入layouts.vue 組件
+import Layouts from '@/components/layouts' // 導入layouts.vue 組件
 import Todo from '@/components/todo' // 導入todo.vue 組件 (陷阱)
 Vue.use(Router) //全局註冊Router組件，它會綁定到Vue實例裡面。
 export default new Router({
   routes: [{
-    path: '/',
-    name: 'Hello',
-    component: layouts,
+    path: '/', //訪問路徑
+    name: 'Hello', // 路徑名
+    component: Layouts, //訪問的組件,即訪問‘/’,它會加載 Layouts 組件所有的內容。
     children: [{
       path: '/todo/:id',
       name: 'todo',
